490b8d2f6f58a7e98d1e9c09ac4d64c29cc53d23
kevinsilva <kevinsilva.j@gmail.com>
2023-05-31 19:09:22 +0100

    adds last solution for unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const calcAverage = (good - bad) / total;
+  const calcPositive = (good / total) * 100;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={calcAverage} />
+        <StatisticLine text="positive" value={`${calcPositive}%`} />
+      </tbody>
+    </table>
   );
-}
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  };
+
+  return (
+    <>
+      <Header title="give feedback" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <Header title="statistics" />
+      {total ? (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </>
+  );
+};
 
 export default App;
