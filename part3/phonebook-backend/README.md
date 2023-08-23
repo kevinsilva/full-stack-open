4da8e3785c62dee3b08e55bebe287dc5ce1dc9b0
kevinsilva <kevinsilva.j@gmail.com>
2023-08-23 19:18:00 +0100

    adds phonebook-backend

---
+++ part3/phonebook-backend/README.md
@@ -0,0 +1,13 @@
+# Phonebook Backend
+
+A Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons.
+
+## Development
+
+To start the application:
+
+```bash
+cd phonebook-backend
+npm install
+npm start
+```
---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+// const generateID = () => {
+//   const maxID =
+//     persons.length > 0 ? Math.max(...persons.map((person) => person.id)) : 0;
+//   return maxID + 1;
+// };
+
+const generateID = () => Math.floor(Math.random() * 999999 + 1);
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
+app.get('/info', (request, response) => {
+  const info = `Phonebook has info for ${persons.length} people`;
+  const date = new Date();
+
+  const infoHtml = `
+        <div>
+            <p>${info}</p>
+            <p>${date}</p>
+        </div>
+    `;
+
+  response.send(infoHtml);
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number)
+    return response.status(404).json({ error: 'content missing' });
+
+  const isPersonRepeated = persons.find((person) => person.name === body.name);
+
+  if (isPersonRepeated)
+    return response.status(409).json({ error: 'Name must be unique' });
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateID(),
+  };
+
+  persons = persons.concat(person);
+  response.json(persons);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
