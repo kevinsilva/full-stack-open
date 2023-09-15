4d115aa560d873e21466004afa4a5534f67b5490
kevinsilva <kevinsilva.j@gmail.com>
2023-09-15 18:28:43 +0100

    adds functionality to database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const app = express();
+require('dotenv').config();
 
-app.use(express.json());
+const Person = require('./models/person');
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name == 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
 app.use(cors());
-
+app.use(express.json());
+app.use(requestLogger);
 app.use(express.static('build'));
 
 morgan.token('post-data', function (request, _) {
@@ -19,55 +43,35 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-const generateID = () => Math.floor(Math.random() * 999999 + 1);
-
 app.get('/api/persons', (_, response) => {
-  response.json(persons);
+  Person.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) response.json(person);
-  else response.status(404).end();
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((foundPerson) => {
+      if (foundPerson) response.json(foundPerson);
+      else response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (_, response) => {
-  const info = `Phonebook has info for ${persons.length} people`;
-  const date = new Date();
+  Person.countDocuments({}).then((count) => {
+    const info = `Phonebook has info for ${count} people`;
+    const date = new Date();
 
-  const infoHtml = `
-        <div>
-            <p>${info}</p>
-            <p>${date}</p>
-        </div>
-    `;
+    const infoHtml = `
+          <div>
+              <p>${info}</p>
+              <p>${date}</p>
+          </div>
+      `;
 
-  response.send(infoHtml);
+    response.send(infoHtml);
+  });
 });
 
 app.post('/api/persons', (request, response) => {
@@ -75,29 +79,48 @@ app.post('/api/persons', (request, response) => {
   if (!body.name || !body.number)
     return response.status(404).json({ error: 'name or number is missing' });
 
-  const isPersonRepeated = persons.find((person) => person.name === body.name);
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-  if (isPersonRepeated)
-    return response.status(409).json({ error: 'name must be unique' });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+  const id = request.params.id;
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateID(),
   };
 
-  persons = persons.concat(person);
-  response.json(persons);
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:_id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params._id)
+    .then((result) => {
+      console.log(result);
+      response.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const URI = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log('Connected to database.');
+  })
+  .catch((err) => {
+    console.log('Error connecting to database: ', err.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);
