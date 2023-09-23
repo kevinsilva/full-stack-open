4862f4838193390ea36536b50ffec8816a261e34
kevinsilva <kevinsilva.j@gmail.com>
2023-09-23 19:39:53 +0100

    adds requests

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,23 +1,10 @@
+const app = require('./app')
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
-})
+});
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = 'mongodb://localhost/bloglist'
-// mongoose.connect(mongoUrl)
-
-// app.use(cors())
-// app.use(express.json())
 
 
