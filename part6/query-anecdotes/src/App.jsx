21131ee09377a645fb01893c0eaed3b717b79a62
kevinsilva <kevinsilva.j@gmail.com>
2024-03-06 18:55:14 +0000

    adds new query-anecdotes w/ new anecdote and vote anecdote functionality

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,68 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, voteAnecdote } from './requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const queryClient = useQueryClient()
+
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: (_, newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      const anecdoteIndex = anecdotes.findIndex((a) => a.id === newAnecdote.id)
+      const newAnecdotes = [
+        ...anecdotes.slice(0, anecdoteIndex),
+        newAnecdote,
+        ...anecdotes.slice(anecdoteIndex + 1),
+      ]
+      queryClient.setQueryData(['anecdotes'], newAnecdotes)
+    },
+  })
+
+  const handleVote = (anecdote) => {
+    const newVote = anecdote.votes + 1
+    voteAnecdoteMutation.mutate({ ...anecdote, votes: newVote })
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <span>anecdote service not available due to problems in server</span>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.content}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
--- part6/redux-anecdotes/src/previousImplementations/mainWithoutToolkit.jsx
+++ part6/redux-anecdotes/src/previousImplementations/mainWithoutToolkit.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+import App from '../App'
+import anecdoteReducer from '../reducers/anecdoteReducer'
+import filterReducer from '../reducers/filterReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
