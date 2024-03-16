14347f668c234e95b3536383f467f52dbf46dfd1
kevinsilva <kevinsilva.j@gmail.com>
2024-03-16 18:37:36 +0000

    implements adding secondary parameter to query allbooks

--- part8/README.md
+++ part8/README.md
@@ -4,4 +4,4 @@ Solutions for the part 8 exercises.
 
 This part of the course is about GraphQL, Facebook's alternative to REST for communication between browser and server.
 
-- **8.1-:** [](.//)
+- **8.1-:** [library-backend](./library-backend/)
--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,7 +107,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => args.author ? books.filter((book) => book.author === args.author) : books,
+    allBooks: (root, args) => {
+      let filteredBooks = books.slice()
+
+      if (args.author) filteredBooks = filteredBooks.filter((book) => book.author === args.author)
+
+      if (args.genre) filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre))
+
+      return filteredBooks
+    }
+    ,
     allAuthors: () => {
       return authors.map((author) => {
         return {
