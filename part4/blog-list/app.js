ee752b97d49e5f78b906e888bca88df13e2f0edf
kevinsilva <kevinsilva.j@gmail.com>
2024-03-16 17:30:07 +0000

    removes extra line

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -26,7 +26,6 @@ app.use(middleware.requestLogger);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 
-
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
