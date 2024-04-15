c537fce43d367688b945886de5642749963cff32
kevinsilva <kevinsilva.j@gmail.com>
2024-04-15 16:56:49 +0100

    changes allBooks query on frontend

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -14,8 +14,15 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title,
-      author,
       published
+      author {
+        name,
+        born,
+        bookCount,
+        id
+      }
+      genres,
+      id
     }
   }
 `
