9090a2029e3632b0fae237acfee77917fa2c3742
kevinsilva <kevinsilva.j@gmail.com>
2024-05-20 16:02:50 +0100

    adds types to backend

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,4 +1,18 @@
-export interface Entry {}
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+}
+
+export type PatientTypes = {
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  dateOfBirth: string;
+  gender: Gender;
+  entries: Entry[];
+};
 
 export type DiagnoseTypes = {
   code: string;
@@ -6,16 +20,55 @@ export type DiagnoseTypes = {
   latin?: string;
 };
 
-export type PatientTypes = {
-  id: string | undefined;
-  name: string;
-  ssn: string;
-  occupation: string;
-  dateOfBirth: string;
-  gender: string;
-  entries: Entry[];
-};
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseTypes['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge?: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthCareEntry
+  | HealthCheckEntry;
 
 export type NewPatientEntryTypes = Omit<PatientTypes, 'id' | 'entries'>;
 
 export type NonSensitivePatientTypes = Omit<PatientTypes, 'ssn' | 'entries'>;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
